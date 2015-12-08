d8bccb319ce18f3226ff6fa01e72eb6765b73589
rennersebastian <renner.sebastian@gmail.com>
2015-12-08 17:09:34 +0100

    Added create Person form and get persons action

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,32 @@
 package controllers;
 
+import models.Person;
 import play.*;
+import play.data.Form;
 import play.mvc.*;
 
+import com.avaje.ebean.Model;
 import views.html.*;
+
+import java.util.List;
+
+import static play.libs.Json.toJson;
 
 public class Application extends Controller {
 
     public Result index() {
         return ok(index.render("This is only a test."));
+    }
+
+    public Result addPerson(){
+        Person person = Form.form(Person.class).bindFromRequest().get();
+        person.save();
+        return redirect(routes.Application.index());
+    }
+
+    public Result getPersons(){
+        List<Person> persons = new Model.Finder<String, Person>(Person.class).all();
+        return ok(toJson(persons));
     }
 
 }
--- app/models/Person.java
+++ app/models/Person.java
@@ -2,8 +2,12 @@ package models;
 
 import com.avaje.ebean.Model;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Person extends Model {
+    @Id
+    public String id;
 
+    public String name;
 }
